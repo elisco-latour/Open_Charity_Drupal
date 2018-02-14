b64a7c8aaf31ef13160ab006f3fbab3f8c4a3ee4
latour4 <eliscolatour@gmail.com>
2018-02-14 20:35:19 +0100

    Wait for the document to load before executing the custom js code

--- oc-main.js
+++ oc-main.js
@@ -1,9 +1,21 @@
-var drawerTrigger = document.querySelector('.oc-drawer__trigger-container');
-var ocOverlay = document.querySelector('.oc-main__overlay');
-var ocDrawer = document.querySelector('.oc-drawer');
-var ocContent = document.querySelector('.oc-content');
-drawerTrigger.addEventListener('click', function(){
-            ocOverlay.classList.toggle('is-active');
-            ocDrawer.classList.toggle('visible-drawer');
-            ocContent.classList.toggle('visible-drawer');
-});
+(function(){
+  /*var ocOverlay = document.querySelector('.oc-main__overlay');
+  var ocDrawer = document.querySelector('.oc-drawer');
+  var ocContent = document.querySelector('.oc-content');
+  drawerTrigger.addEventListener('click', function(){
+              ocOverlay.classList.toggle('is-active');
+              ocDrawer.classList.toggle('visible-drawer');
+              ocContent.classList.toggle('visible-drawer');
+  });*/
+  window.addEventListener("load", function(event) {
+    var drawerTrigger = document.querySelector('.oc-drawer__trigger-container');
+    var ocOverlay = document.querySelector('.oc-main__overlay');
+    var ocDrawer = document.querySelector('.oc-drawer');
+    var ocContent = document.querySelector('.oc-content');
+    drawerTrigger.addEventListener('click', function(){
+                ocOverlay.classList.toggle('is-active');
+                ocDrawer.classList.toggle('visible-drawer');
+                ocContent.classList.toggle('visible-drawer');
+    });
+  });
+})();
